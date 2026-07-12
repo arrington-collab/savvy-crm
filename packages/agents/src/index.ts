3c9e720815d3220bcc7e88b97e4fc59ff4113e90
Arrington <arrington@advosy.com>
2026-07-12 15:33:49 -0700

    feat(agents): hourly challenge settlement cron (backstop)

--- packages/agents/src/index.ts
+++ packages/agents/src/index.ts
@@ -41,6 +41,7 @@ import { parseSupplierInvoice } from "./functions/supplier-invoice-parse";
 import { priceGuardSupplierInvoice } from "./functions/supplier-invoice-guard";
 import { canvassContractSigned } from "./functions/canvass-contract";
 import { parseLeadDocument } from "./functions/parse-lead-document";
+import { challengeSettleHourly } from "./functions/challenge-settle";
 
 export { inngest } from "./client";
 export { examplePing } from "./functions/example";
@@ -92,4 +93,5 @@ export { priceGuardSupplierInvoice, priceGuardHandler } from "./functions/suppli
 export { getTenantSms } from "./telephony";
 export { canvassContractSigned, storeCanvassContract, emailSignedCopy } from "./functions/canvass-contract";
 export { parseLeadDocument, parseLeadDocumentHandler } from "./functions/parse-lead-document";
-export const functions = [voiceFallback, examplePing, leadIntake, jobStageChanged, dripRun, appointmentCalendarSync, appointmentReminders, dunningRun, commissionOnPaid, referralFeeOnPaid, qboPushInvoice, qboPushPayment, esignFinalize, roofrOrderMeasurement, autoOrderMeasurementOnInspection, generateEstimateOnMeasurement, sendEstimateForSignature, estimateAcceptedAdvanceJob, createMaterialOrderOnAccepted, meterUsageMonthly, coldArchiveDocuments, enrichmentSweep, taskHealthSweep, opsDigest, emailAppendSweep, changeOrderAccepted, stormCertOnLead, stormCertOnLeadFailure, leadRescore, leadCalibrationMonthly, leadSpeedToLead, leadCadence, invoiceSentToBilling, invoicePaidToComplete, crewCheckedInToProduction, materialDeliveredToProduction, productionPhotosToCloseout, homeownerCrewNotify, homeownerDeliveryNotify, retailCloseoutCadence, insuranceDepreciationDetect, weatherReschedule, homeownerNotify, photoQc, parseSupplierInvoice, priceGuardSupplierInvoice, canvassContractSigned, parseLeadDocument, stormAlertDaily];
+export { challengeSettleHourly } from "./functions/challenge-settle";
+export const functions = [voiceFallback, examplePing, leadIntake, jobStageChanged, dripRun, appointmentCalendarSync, appointmentReminders, dunningRun, commissionOnPaid, referralFeeOnPaid, qboPushInvoice, qboPushPayment, esignFinalize, roofrOrderMeasurement, autoOrderMeasurementOnInspection, generateEstimateOnMeasurement, sendEstimateForSignature, estimateAcceptedAdvanceJob, createMaterialOrderOnAccepted, meterUsageMonthly, coldArchiveDocuments, enrichmentSweep, taskHealthSweep, opsDigest, emailAppendSweep, changeOrderAccepted, stormCertOnLead, stormCertOnLeadFailure, leadRescore, leadCalibrationMonthly, leadSpeedToLead, leadCadence, invoiceSentToBilling, invoicePaidToComplete, crewCheckedInToProduction, materialDeliveredToProduction, productionPhotosToCloseout, homeownerCrewNotify, homeownerDeliveryNotify, retailCloseoutCadence, insuranceDepreciationDetect, weatherReschedule, homeownerNotify, photoQc, parseSupplierInvoice, priceGuardSupplierInvoice, canvassContractSigned, parseLeadDocument, stormAlertDaily, challengeSettleHourly];
